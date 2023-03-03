999a49c25f54e9e9a66e9b63d4dc0b1814b372d1
Nick Castello <ncastello@thefarmersdog.com>
2023-03-03 10:38:49 -0500

    Fixed spacing in template. Made template generation generic and dynamic. Added gitignore

--- index.py
+++ index.py
@@ -4,17 +4,20 @@ from utils import *
 import os
 
 OUTPUT_DIR = "./output"
+TEMPLATES_DIR = "./templates"
 
 # Remove all files in output dir
 for file in os.scandir(OUTPUT_DIR):
     os.remove(file.path)
 
 
-environment = Environment(loader=FileSystemLoader("templates/"))
+environment = Environment(
+    loader=FileSystemLoader(TEMPLATES_DIR), trim_blocks=True, lstrip_blocks=True
+)
 # userInputObj = getUserInput()
 userInputObj = getTestData()
 
-templateNames = ["type.txt", "mutation.txt", "mutation.medium-test.txt"]
+templateNames = os.listdir(TEMPLATES_DIR)
 
 for templateName in templateNames:
     template = environment.get_template(templateName)
@@ -25,46 +28,6 @@ for templateName in templateNames:
     with open(os.path.join(OUTPUT_DIR, fileName), mode="w", encoding="utf-8") as file:
         file.write(content)
         print(f"... wrote {fileName}")
-
-# # Generate _.type.ts
-# typeFileTemplate = environment.get_template("type.txt")
-# typeFileName = f"{userInputObj.getFileNamePrefix()}.type.ts"
-
-# content = typeFileTemplate.render({"data": userInputObj})
-
-# with open(os.path.join(OUTPUT_DIR, typeFileName), mode="w", encoding="utf-8") as file:
-#     file.write(content)
-#     print(f"... wrote {typeFileName}")
-
-
-# # Generate _.mutation.ts
-# mutationFileTemplate = environment.get_template("mutation.txt")
-# mutationFileName = f'{fileNamePrefix}.mutation.ts'
-
-# content = mutationFileTemplate.render({
-#     'endpoint_name': endpointName,
-#     'endpoint_full_name': endpointFullName,
-#     'type_file_name_for_import': getFileNameWithoutExtension(typeFileName),
-#   }
-# )
-
-# with open(os.path.join(OUTPUT_DIR, mutationFileName), mode="w", encoding="utf-8") as file:
-#     file.write(content)
-#     print(f"... wrote {mutationFileName}")
-
-
-# # Generate _.mutation.medium-test.ts
-# mutationMediumTestFileTemplate = environment.get_template("mutation.medium-test.txt")
-# mutationMediumTestFileName = f'{fileNamePrefix}.mutation.medium-test.ts'
-
-# content = mutationMediumTestFileTemplate.render({
-#     'endpoint_name': endpointName
-#   }
-# )
-
-# with open(os.path.join(OUTPUT_DIR, mutationMediumTestFileName), mode="w", encoding="utf-8") as file:
-#     file.write(content)
-#     print(f"... wrote {mutationMediumTestFileName}")
 
 
 # TODO: Fix module not found error in userInput.py with importing field type
